2e81ae91fe39f9149e7b41ad7f4f47bca2480df1
leshnik-t <lenatl@gmail.com>
2023-11-01 11:32:21 +0200

    stretch goals

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -7,7 +7,7 @@ const Card = ({props}) => {
 
     return (
         <div className="col" >
-            <div className={`card ${props.category.toLowerCase()} h-100`}>
+            <div className={`card ${props.category.toLowerCase()} ${props.featured ? "featured" : null} h-100`}>
                 {props.category !== '' &&
                     <div className="card-header">
                         {props.category}
@@ -17,12 +17,12 @@ const Card = ({props}) => {
                     <img src={props.image} className="card-img-top" alt={props.title}/>
                 </div>
                 <div className="card-body">
-                    <h3 className="card-title">{props.title}</h3>
+                    <h3 className="card-title">{props?.title ? props.title : 'National Park'}</h3>
                     <p className="card-text">{props.body}</p>
                 </div>
                 { props.link !== '' &&
                 <div className="card-footer">
-                    <a href={props.link}>{props.linkText}</a>
+                    <a href={props.link} target="_blank" rel="noreferrer">{props.linkText}</a>
                 </div>
                 }
             </div>
--- src/components/cardsContainer/CardsContainer.js
+++ src/components/cardsContainer/CardsContainer.js
@@ -2,15 +2,22 @@ import './cards-container.scss';
 import Card from '../card/Card';
 
 const CardsContainer = ({data}) => {
-    console.log(data);
+    const featuredCards = data[Object.keys(data)[0]].filter((cardData) => cardData.featured === true);
+
+    const cards = data[Object.keys(data)[0]].filter((cardData) => cardData.featured !== true);
+
+    const cardsArray = [...featuredCards, ...cards];
+
+    console.log(cardsArray);
+
     return (
         <section className="cards-container">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col">
                         <h2>{Object.keys(data)[0]} category</h2>
-                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 g-4">
-                            {data[Object.keys(data)[0]].map((cardData) => (
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 g-3">
+                            {cardsArray.map((cardData) => (
                                 <Card key={cardData.id} props={cardData}/>
                             ))}
                         </div>
